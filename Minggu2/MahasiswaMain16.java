e9095c947c1583add63562bfea3109baf94fcd0c
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2026-02-20 10:48:25 +0700

    update percobaan 3

--- Minggu2/MahasiswaMain16.java
+++ Minggu2/MahasiswaMain16.java
@@ -1,11 +1,6 @@
 public class MahasiswaMain16 {
     public static void main(String[] args) {
         Mahasiswa16 mhs1 = new Mahasiswa16("Muhamad Rizky Hamdani", "254107020027", "TI-1C", 3.5);
-        mhs1.nama = "Muhamad Rizky Hamdani";
-        mhs1.nim = "254107020027";
-        mhs1.kelas = "TI-1C";
-        mhs1.ipk = 3.5;
-
         mhs1.tampilkaninformasi();
         mhs1.ubahKelas("TI-1D");
         mhs1.updateipk(3.8);
