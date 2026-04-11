f0a56a26e9575899dfe09933a5f28122d115074c
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2026-04-11 13:14:28 +0700

    comment commit

--- Minggu6/MahasiswaBerprestasi16.java
+++ Minggu6/MahasiswaBerprestasi16.java
@@ -20,17 +20,17 @@ public class MahasiswaBerprestasi16 {
 
     void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
-            System.out.println("iterasi ke-" + i);
+            // System.out.println("iterasi ke-" + i);
             for (int j = 1; j < listMhs.length - i; j++) {
                 if (listMhs[j - 1].ipk < listMhs[j].ipk) {
                     Mahasiswa16 tmp = listMhs[j-1];
                     listMhs[j-1] = listMhs[j];
                     listMhs[j] = tmp;
                 }
-                for (Mahasiswa16 mahasiswa16 : listMhs) {
-                mahasiswa16.tampilIPK();
-                }
-                System.out.println();
+                // for (Mahasiswa16 mahasiswa16 : listMhs) {
+                // mahasiswa16.tampilIPK();
+                // }
+                // System.out.println();
             }
         }
     }
